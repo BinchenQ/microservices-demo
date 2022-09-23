ed5a39f8109af824589211b5155078ad0c8d4c0c
Chen, Bin (C|TN-P2) <bin.chen1@mobilityasia.com.cn>
2022-09-23 15:15:18 +0800

    off dash

--- hack/make-release.sh
+++ hack/make-release.sh
@@ -8,7 +8,6 @@
 
 set -e
 SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-[[ -n "${DEBUG:-}" ]] && set -x
 
 log() { echo "$1" >&2; }
 fail() { log "$1"; exit 1; }
@@ -16,7 +15,7 @@ fail() { log "$1"; exit 1; }
 VERSION="${1:-${TAG:?TAG env variable must be specified}}"
 REPO_PREFIX="${REPO_PREFIX:?REPO_PREFIX env variable must be specified}"
 
-if [[ "$VERSION" != v* ]]; then
+if [ "$VERSION" != v* ]; then
     fail "\$VERSION must start with 'v', e.g. v0.1.0 (got: $VERSION)"
 fi
 
--- makefile
+++ makefile
@@ -17,5 +17,8 @@ rls:
 	# - 4. pushing the tag/commit to main.
 	TAG=$(TAG) REPO_PREFIX=$(REPO_PREFIX) sh hack/make-release.sh $(TAG)
 
+init:
+	# ubuntu default use dash which has too much problems
+	sudo ln -fs /bin/bash /bin/sh
 recover:
  	export https_proxy=127.0.0.1:44787
